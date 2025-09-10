7b394e740fe6dafcde271af2e91d1ce7a901fc19
Heja <arslanheja202@gmail.com>
2025-09-10 03:10:54 +0300

    feat(abstract classes) create abstract class and implement to user repository

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,20 +1,46 @@
-import { User } from 'src/models';
+import { Project, User } from 'src/models';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import type {
   UserCreationAttributes,
   UserModelStatic,
+  UserUpdateAttributes,
 } from 'src/types/type.user-repository';
+import { Repository } from './repository.base';
 
 @Injectable()
-export class UserRepository {
-  async create(args: UserCreationAttributes): Promise<User> {
-    return await this.userModel.create(args);
+export class UserRepository extends Repository<User> {
+  async create(creationInterface: UserCreationAttributes): Promise<User> {
+    return await this.userModel.create(creationInterface);
   }
 
-  async findOne(_email: string): Promise<User | null> {
-    return await this.userModel.findOne({ where: { email: _email } });
+  async findByPk(_id: number): Promise<User | null> {
+    return await this.userModel.findByPk(_id);
   }
 
-  constructor(@InjectModel(User) private readonly userModel: UserModelStatic) {}
+  async find(): Promise<User[] | null> {
+    return await this.userModel.findAll();
+  }
+
+  async update(
+    updateInterface: UserUpdateAttributes,
+    _id: number,
+  ): Promise<User | any> {
+    return await this.userModel.update(
+      { ...updateInterface },
+      { where: { _id: _id } },
+    );
+  }
+
+  async findByEmail(_email: string): Promise<User | null> {
+    return await this.userModel.findOne({
+      where: {
+        email: _email,
+      },
+    });
+  }
+
+  constructor(@InjectModel(User) private readonly userModel: UserModelStatic) {
+    super();
+  }
 }
